e25c39f2703bf4bed628e1611d75afe6f8720382
Daniel St. Clair <danielallanstclair@gmail.com>
2015-02-18 11:13:20 -0600

    deleting clutter

--- tabs/scripts/controllers.js
+++ tabs/scripts/controllers.js
@@ -1,35 +1,3 @@
-// angular.module('app.controller', []).controller('TabsCtrl', function ($scope) {
-
-// 	// $scope.currentTab === 'One'
-// 	// $scope.showTabs = function(currentTab){
-// 	// 	$scope.currentTab = $scope.currentTab
-// 	// }
-
-// 	$scope.showFirstTab = false;
-// 	$scope.showSecondTab = false;
-// 	$scope.showThirdTab = false;
-
-
-
-// 	$scope.showFirst = function(){
-// 			$scope.showFirstTab = true;
-// 			$scope.showSecondTab = false;
-// 			$scope.showThirdTab = false;
-// 	}
-// 	$scope.showSecond = function(){
-// 		$scope.showFirstTab = false;
-// 		$scope.showSecondTab = true;
-// 		$scope.showThirdTab = false;
-// 	}
-// 	$scope.showThird = function(){
-// 		$scope.showFirstTab = false;
-// 		$scope.showSecondTab = false;
-// 		$scope.showThirdTab = true;
-// 	}
-
-
-// })
-
 angular.module('app.controller', []).controller('TabsCtrl', ['$scope', function($scope) {
 	$scope.currentTab = 'one';
 	$scope.changeTab = function(num) {
@@ -37,29 +5,3 @@ angular.module('app.controller', []).controller('TabsCtrl', ['$scope', function(
 		$scope.currentTab = num;
 	}
 }]);
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
